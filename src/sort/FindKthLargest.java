a8ba5b6a6a7e57217b130500c3334edc90ed181e
Royee <2854919383@qq.com>
2020-04-21 23:52:52 +0800

    更新leetcode

--- src/sort/FindKthLargest.java
+++ src/sort/FindKthLargest.java
@@ -3,39 +3,31 @@ package sort;
 public class FindKthLargest {
     static int index = 0;
     public static int findKthLargest(int[] nums, int k) {
-        int left = 0, right = nums.length - 1;
-        quickSort(nums, left, right, k);
-        return nums[index];
+        for (int i = nums.length / 2 - 1; i >= 0; i--) {
+            adjustHeap(nums, i, nums.length);
+        }
+        for (int j = nums.length - 1; j >= nums.length - k; j--) {
+            int temp = nums[j];
+            nums[j] = nums[0];
+            nums[0] = temp;
+            adjustHeap(nums, 0, j);
+        }
+        return nums[nums.length - k];
     }
-    public static void quickSort(int[] nums, int left, int right, int k) {
-        if(left < right) {
-            int i = left;
-            int j = right;
-            int temp = nums[i];
-            while(i < j) {
-                while(i < j && nums[j] > temp){
-                    j--;
-                }
-                if(i < j){
-                    nums[i] = nums[j];
-                    i++;
-                }
-                while(i < j && nums[i] < temp) {
-                    i++;
-                }
-                if(i < j){
-                    nums[j] = nums[i];
-                    j--;
-                }
-                nums[i] = temp;
-                if(nums.length - i == k) {
-                    index = i;
-                    return;
-                }
-                quickSort(nums, left, i - 1, k);
-                quickSort(nums, i + 1, right, k);
+    public static void adjustHeap(int[] arr, int i, int length) {
+        int temp = arr[i];
+        for (int k = i*2 + 1; k < length; k = k*2 + 1) {
+            if(k + 1 < length && arr[k] < arr[k+1]){
+                k = k + 1;
+            }
+            if(arr[k] > temp) {
+                arr[i] = arr[k];
+                i = k;
+            }else {
+                break;
             }
         }
+        arr[i] = temp;
     }
 
     public static void main(String[] args) {
